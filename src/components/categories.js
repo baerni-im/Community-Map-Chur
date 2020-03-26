b6a56dca5028b709a2b6f3e52d8a4d0c5e6e4ea6
baerni-im <61183169+baerni-im@users.noreply.github.com>
2020-03-26 12:19:16 +0100

    farbe test

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,5 +1,5 @@
 module.exports = [
-	{ ident: 'kinderbetreuung', text: 'Kinderbetreuung', color: '#1289A7' },
+	{ ident: 'kinderbetreuung', text: 'Kinderbetreuung', color: '#088A29' },
 	{ ident: 'food delivery', text: 'Food Delivery', color: '#D980FA' },
 	{ ident: 'takeaway', text: 'Food Take-Away', color: '#30336b' },
 	{ ident: 'shop', text: 'Happy to shop for you', color: '#8e44ad' },
