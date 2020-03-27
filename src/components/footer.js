d70ed475c489f750ee96051b4760053f16e52109
baerni-im <61183169+baerni-im@users.noreply.github.com>
2020-03-27 10:35:01 +0100

    footer anpassung

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
   return (
     <footer className='ui vertical inverted segment'>
       <div className='ui center aligned container'>
-        <i className='ui icon copyright' />{new Date().getFullYear()}{' '}{data.site.siteMetadata.title}{' ' }|{' '}<a href={`mailto:${data.site.siteMetadata.email}`}><i className='ui icon envelope' />Schreib eine Mail</a>{' '}|{' '}<a href={`https://twitter.com/baern_i`} target={'_blank'} rel={'noopener noreferrer'}><i className='ui icon twitter' />Folge mir auf Twitter</a>{' '}|{' '} Mit <i className={'ui icon heart'} /> erstellt von Bernhard Aebersold - mit der Vorlage von <a href='https://twitter.com/@mrcfhr' target={'_blank'} rel={'noopener noreferrer'}>Marc Fehr</a>{' ' }|{' '}<a href={`https://churhilftchur.ch/impressum/`} rel={'noopener noreferrer'}>Impressum</a>
+        <i className='ui icon copyright' />{new Date().getFullYear()}{' '}{data.site.siteMetadata.title}{' ' }|{' '}<a href={`https://twitter.com/baern_i`} target={'_blank'} rel={'noopener noreferrer'}><i className='ui icon twitter' />Folge mir auf Twitter</a>{' '}|{' '} Mit <i className={'ui icon heart'} /> erstellt von <a href='bernhardaebersold.ch' target={'_blank'} rel={'noopener noreferrer'}>Bernhard Aebersold</a> - mit der Vorlage von <a href='https://twitter.com/@mrcfhr' target={'_blank'} rel={'noopener noreferrer'}>Marc Fehr</a>{' ' }|{' '}<a href={`https://churhilftchur.ch/impressum/`} rel={'noopener noreferrer'}>Impressum</a>
       </div>
     </footer>
   );
