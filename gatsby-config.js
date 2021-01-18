9b7b04526cd1ffbe7090c4cf501b9208a81a03ee
baerni-im <beni.aebersold@gmail.com>
2021-01-18 11:16:36 +0100

    Löschen von Gutescheinen
    Die Junge Wirtschaftskammer hat ihr Angebot mit den Gutscheinen nicht mehr online.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
     menuLinks: [
       {title: 'Home', link: '/', icon: 'map'},
       {title: 'Hinzufügen', link: '/add', icon: 'plus'},
-      {title: 'Mit Gutschein helfen', link: '/jetzt_helfen', icon: 'heart'},
       {title: 'Kontakt', link: '/contact', icon: 'info'},
 
     ],
